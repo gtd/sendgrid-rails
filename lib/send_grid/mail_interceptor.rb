c6c47d6d974f5cde7c14af9301e2be0488aa6d95
Gabe da Silveira <gabe@websaviour.com>
2012-07-02 11:09:33 +0100

    Add explicit error for AM 2.0 API usage

--- lib/send_grid/mail_interceptor.rb
+++ lib/send_grid/mail_interceptor.rb
@@ -2,9 +2,14 @@ module SendGrid
   class MailInterceptor
     def self.delivering_email(mail)
       sendgrid_header = mail.instance_variable_get(:@sendgrid_header)
-      sendgrid_header.add_recipients(mail.to)
-      mail.header['X-SMTPAPI'] = sendgrid_header.to_json if sendgrid_header.data.present?
-      mail.header['to'] = 'dummy@email.com'
+
+      if sendgrid_header.nil?
+        raise "sendgrid_header not found. Are you using the deprecated ActionMailer 2 API? You must call the ActionMailer 3.0 mail() method for sendgrid-rails to work"
+      else
+        sendgrid_header.add_recipients(mail.to)
+        mail.header['X-SMTPAPI'] = sendgrid_header.to_json if sendgrid_header.data.present?
+        mail.header['to'] = 'dummy@email.com'
+      end
     end
   end
 end
